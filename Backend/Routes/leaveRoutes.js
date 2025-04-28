440778d3580336e1cf1d642046088d3ba2c161be
Om Raja <omraja451@gmail.com>
2025-04-29 00:44:33 +0530

    feat: add show my leave request route for user

--- Backend/Routes/leaveRoutes.js
+++ Backend/Routes/leaveRoutes.js
@@ -1,8 +1,9 @@
 const isLoggedIn = require("../middlewares/isLoggedIn");
-const { applyForLeave , updateLeaveRequest} = require("../controllers/leaveController");
+const { applyForLeave , updateLeaveRequest, showMyLeaveRequest} = require("../controllers/leaveController");
 const router = require("express").Router();
 
 router.post("/", isLoggedIn, applyForLeave);
+router.get("/", isLoggedIn, showMyLeaveRequest);
 router.patch("/:id", isLoggedIn, updateLeaveRequest);
 
 module.exports = router;
