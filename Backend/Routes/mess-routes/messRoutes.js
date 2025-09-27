06a1f1632eb7685ec9204ca32597ad7a21ce241d
Om Raja <omraja451@gmail.com>
2025-09-28 04:51:10 +0530

    fix: incorrect data in monthly bill

--- Backend/Routes/mess-routes/messRoutes.js
+++ Backend/Routes/mess-routes/messRoutes.js
@@ -4,8 +4,8 @@ const isLoggedIn = require("../../middlewares/isLoggedIn");
 const {isMessManager} = require("../../middlewares/checkRole.js");
 const {showBills, addBill, getBill} = require("../../controllers/mess/billController.js");
 
-router.get("/", isLoggedIn, isMessManager, getBill);
-router.get("/monthlyBill", isLoggedIn, isMessManager, showBills);
+router.get("/", isLoggedIn, isMessManager, showBills);
+router.get("/monthlyBill", isLoggedIn, isMessManager, getBill);
 router.post("/:id", isLoggedIn, isMessManager, addBill);
 
 module.exports = router;
