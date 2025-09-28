57f5e1237ee89884e7f29fd794ea685dd48ca47a
Om Raja <omraja451@gmail.com>
2025-09-28 05:58:30 +0530

    feat: add role change route and controller to superAdmin

--- Backend/Routes/super-admin/superAdminRoute.js
+++ Backend/Routes/super-admin/superAdminRoute.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const isLoggedIn = require("../../middlewares/isLoggedIn");
 const { isSuperAdmin } = require("../../middlewares/checkRole.js");
-const {fetchEmployee} = require("../../controllers/superAdmin/superAdminController.js");
+const {fetchEmployee, assignRole}= require("../../controllers/superAdmin/superAdminController.js");
 
 router
   .route("/")
-  .get(isLoggedIn, isSuperAdmin, fetchEmployee);
+  .get(isLoggedIn, isSuperAdmin, fetchEmployee)
+  .patch(isLoggedIn, isSuperAdmin, assignRole);
 
 module.exports = router;
