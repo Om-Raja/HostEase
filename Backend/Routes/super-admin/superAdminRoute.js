8c2dc8c8428407e9e02d0c12cc81e1c21b571cc9
Om Raja <omraja451@gmail.com>
2025-09-28 06:25:42 +0530

    fix: bug in fetching employee detail

--- Backend/Routes/super-admin/superAdminRoute.js
+++ Backend/Routes/super-admin/superAdminRoute.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const isLoggedIn = require("../../middlewares/isLoggedIn");
 const { isSuperAdmin } = require("../../middlewares/checkRole.js");
-const {fetchEmployee, assignRole}= require("../../controllers/superAdmin/superAdminController.js");
+const {fetchEmployee, assignRole, removeRole} = require("../../controllers/superAdmin/superAdminController.js");
 
 router
   .route("/")
   .get(isLoggedIn, isSuperAdmin, fetchEmployee)
-  .patch(isLoggedIn, isSuperAdmin, assignRole);
+  .patch(isLoggedIn, isSuperAdmin, assignRole)
+  .delete(isLoggedIn, isSuperAdmin, removeRole);
 
 module.exports = router;
