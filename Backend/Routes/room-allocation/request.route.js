1f8e5c1394fbe2c91d1c91f23aabf230d46ad9d8
Om Raja <omraja451@gmail.com>
2025-11-09 12:06:29 +0530

    add check role === caretaker for sensitive routes

--- Backend/Routes/room-allocation/request.route.js
+++ Backend/Routes/room-allocation/request.route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const {showRoomRequest, sendRoomRequest, actOnRoomRequest} = require("../../controllers/room/room.controller.js");
 
 router.post("/", sendRoomRequest);
-router.get("/", showRoomRequest); //todo: add iscaretaker role check here
-router.post("/:reqId", actOnRoomRequest); //todo: add is caretaker role check here also
+router.get("/", isCareTaker, showRoomRequest); 
+router.post("/:reqId", isCareTaker, actOnRoomRequest); 
 
 module.exports = router;
--- Backend/Routes/room-allocation/room.route.js
+++ Backend/Routes/room-allocation/room.route.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const {addRoom, getAllRoomData, deleteRoom, editRoom} = require("../../controllers/room/room.controller.js");
+const { isCareTaker } = require("../../middlewares/checkRole.js");
 
-//todo: add check role === caretaker middleware here
-router.post("/", addRoom);
-router.get("/", getAllRoomData);
-router.patch("/", editRoom);
-router.delete("/", deleteRoom);
+router.post("/", isCareTaker, addRoom);
+router.get("/", isCareTaker, getAllRoomData);
+router.patch("/", isCareTaker, editRoom);
+router.delete("/", isCareTaker, deleteRoom);
 
 module.exports = router;
